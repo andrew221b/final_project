0ce70f87791dfc6069235b950176852bac1eb87e
andrew221b <andrewguitarr@gmail.com>
2022-09-05 19:41:50 +0300

    home

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,36 @@
-/* JavaScript */
-  $('.header-menu__btn').on('click', function () {
-    $(this).toggleClass('header-menu__btn--active');
-    $('.header-menu__list').stop(true, true).fadeToggle(500);
-  });
+let $btnHamburger = $('.header-menu__btn');
+let $navbarList = $('.header-menu__list');
+let $navbarLink = $('.header-menu__link');
+let $navbarItem = $('.header-menu__item');
+let $navbarBtn = $('.header-menu__btn-row');
+let $window = $(window);
+let $body = $('body');
+
+let toggleActive = () => {
+  $btnHamburger.toggleClass('burger');
+  $navbarList.toggleClass('burger');
+  $navbarLink.toggleClass ('burger');
+  $navbarItem.toggleClass ('burger');
+  $navbarBtn.toggleClass ('burger');
+};
+
+let resetMenu = () => {
+  $btnHamburger.removeClass('burger');
+  $body.removeClass('burger');
+  $navbarList.removeClass('burger');
+  $navbarLink.removeClass ('burger');
+  $navbarItem.removeClass ('burger');
+  $navbarBtn.removeClass ('burger');
+};
+
+$btnHamburger.on('click', () => {
+  toggleActive();
+  $body.toggleClass('burger');
+});
+
+$window.on('resize', () => {
+  if ($window.width() > 600) {
+      resetMenu();
+  }
+});
+
