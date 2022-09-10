40783190e45e8d2cd637720bc0e6ec2000fad463
andrew221b <andrewguitarr@gmail.com>
2022-09-10 22:58:49 +0300

    add switcher

--- assets/js/main.js
+++ assets/js/main.js
@@ -66,3 +66,58 @@ heightMax = Math.max(...heightsArr);
 descriptionsList.forEach(el => el.style.height = `${heightMax}px`);
 
 
+
+
+class Switch {
+  constructor(
+    selector,
+    { highlightClass = "topics__switch-highlight", activeClass = "is-active" } = {}
+  ) {
+    this.activeClass = activeClass;
+    this.element = document.querySelector(selector);
+    this.buttons = this.element.querySelectorAll(".topics__switch-btn");
+    this.highlight = this.element.querySelector(`.${highlightClass}`);
+    this.activeBtn = this.element.querySelector(`topics__switch-btn.${this.activeClass}`);
+
+    if (!this.activeBtn) {
+      this.activeBtn = this.buttons[0];
+      this.activeBtn.classList.add(this.activeClass);
+    }
+
+    this._highlight();
+    this._addEvents();
+  }
+
+  _highlight() {
+    const w = this.activeBtn.offsetWidth;
+    const x = this.activeBtn.offsetLeft;
+
+    this.highlight.style.width = `${w}px`;
+    this.highlight.style.transform = `translateX(${x}px)`;
+  }
+
+  _dispatchEvent() {
+    this.element.dispatchEvent(
+      new CustomEvent("change", { detail: this.activeBtn.dataset.value })
+    );
+  }
+
+  _addEvents() {
+    [].forEach.call(this.buttons, btn =>
+      btn.addEventListener("click", e => {
+        if (this.activeBtn === e.target) return;
+
+        this.activeBtn.classList.remove(this.activeClass);
+        this.activeBtn = e.target;
+        this.activeBtn.classList.add(this.activeClass);
+
+        this._highlight();
+        this._dispatchEvent();
+      })
+    );
+  }
+}
+
+const mySwitch = new Switch(".topics__switch");
+
+mySwitch.element.addEventListener("change", e => console.log(e.detail));
